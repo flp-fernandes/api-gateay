269e8b0f171df536562fa254c49c5cca5cddbcc5
Felipe Fernandes <felipe.fernandes@somosphi.com>
2021-08-31 11:24:47 -0300

    criantion of api-gateway

---
+++ src/app.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+
+@Controller('api/v1')
+export class AppController {
+  private logger = new Logger(AppController.name);
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://guest:guest@localhost:5672/smartranking'],
+        queue: 'admin-backend',
+      },
+    });
+
+    this.onApplicationBootstrap();
+  }
+
+  async onApplicationBootstrap() {
+    await this.clientAdminBackend.connect();
+  }
+
+  @Get('categorias')
+  consultarCategorias(@Query('idCategoria') _id: string): Observable<any> {
+    return this.clientAdminBackend.send('consultar-categorias', _id ? _id : '');
+  }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
+  }
+}
